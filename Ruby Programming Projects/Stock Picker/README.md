0a76ebdfc2bb8136a600c44ee80d3dd9fcdc1dd1
Miguel <jmsoliv95@gmail.com>
2015-06-26 01:44:05 +0100

    Solved stock_picker

---
+++ Ruby Programming Projects/Stock Picker/README.md
@@ -0,0 +1,4 @@
+# Project 2: Stock Picker
+
+## How to run
+ruby stock_picker.rb
--- Ruby Programming Projects/Stock Picker/stock_picker.rb
+++ Ruby Programming Projects/Stock Picker/stock_picker.rb
@@ -1,1 +1,15 @@
-puts "test"
+def stock_picker prices
+  price, sell, buy = 0, 0, 0
+  (0...prices.size).each do |i|
+    (i+1...prices.size).each do |j|
+      if (prices[j]-prices[i]) > price
+        price = (prices[j]-prices[i])
+        buy = i
+        sell = j
+      end
+    end
+  end
+  puts "[#{buy}, #{sell}]"
+end
+
+stock_picker [17,3,6,9,15,8,6,1,10]
