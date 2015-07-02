ee4bdc086b593d12ed4f6a7b3543eb66dc44924f
Miguel <jmsoliv95@gmail.com>
2015-07-03 00:51:24 +0100

    Solved Serialization project

--- Ruby Programming Projects/Project: Serialization and Working with Files/EventManager/lib/event_manager.rb
+++ Ruby Programming Projects/Project: Serialization and Working with Files/EventManager/lib/event_manager.rb
@@ -1,37 +1,44 @@
 require 'csv'
+require 'erb'
 require 'sunlight/congress'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-def clean_zipcode zipcode
+def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0...5]
 end
 
-def legislators_to_s legislators
-  legislators_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
+def legislators_by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
-def legislators_by_zipcode zipcode
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legislators_to_s(legislators).join(", ")
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 def main
   puts "EventManager Initialized"
-  template_letter = File.read "form_letter.html"
+  template_letter = File.read "form_letter.erb"
+  erb_template = ERB.new template_letter
 
   contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
   contents.each do |row|
+    id = row[0]
     name = row[:first_name]
+
     zipcode = clean_zipcode(row[:zipcode])
+
     legislators = legislators_by_zipcode zipcode
 
-    personal_letter = template_letter.gsub('FIRST_NAME',name)
-    personal_letter.gsub!('LEGISLATORS',legislators)
+    form_letter = erb_template.result(binding)
 
-    puts personal_letter
+    save_thank_you_letters(id, form_letter)
   end
 end
 main
