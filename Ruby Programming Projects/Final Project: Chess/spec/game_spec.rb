c5ce108c407e2f1ed61735b0af35c0a2fea26b42
Miguel <jmsoliv95@gmail.com>
2015-09-19 22:51:28 +0100

    add get_piece_at tests

--- Ruby Programming Projects/Final Project: Chess/spec/game_spec.rb
+++ Ruby Programming Projects/Final Project: Chess/spec/game_spec.rb
@@ -6,9 +6,9 @@ describe Game do
   describe "#move_piece" do
     it "moves to and from the correct positions" do
       comparison_piece = game.board[6][Game::LETTERS_TO_INDEX['A']]
-      game.move_piece("6A", "2B")
+      game.move_piece("6A", "5A")
       expect(game.board[6][Game::LETTERS_TO_INDEX['A']]).to eq('-')
-      expect(game.board[2][Game::LETTERS_TO_INDEX['B']]).to eq(comparison_piece)
+      expect(game.board[5][Game::LETTERS_TO_INDEX['A']]).to eq(comparison_piece)
     end
     it "does nothing if there is no piece at the desired square" do
       expect(game.move_piece("4A", "3A")).to be_falsey
@@ -19,6 +19,16 @@ describe Game do
       expect(game.out_of_bounds("6A", "8A")).to be_truthy
       expect(game.out_of_bounds("6A", "6I")).to be_truthy
       expect(game.out_of_bounds("6A", "5A")).to be_falsey
+    end
+  end
+
+  describe "#get_piece_at" do
+    it "returns the name of the correct piece" do
+      game.move_piece("6A", "5A")
+      expect(game.get_piece_at("6A")).to eq("-")
+      expect(game.get_piece_at("5A")).to eq("PAWN_BLACK")
+      expect(game.get_piece_at("7H")).to eq("ROOK_BLACK")
+      expect(game.get_piece_at("0E")).to eq("QUEEN_WHITE")
     end
   end
 end
