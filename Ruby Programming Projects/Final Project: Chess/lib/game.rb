e1bc3dc8703658d0d6354568defb37ba066cabee
Miguel <jmsoliv95@gmail.com>
2015-09-19 22:35:16 +0100

    add out_of_bounds function

--- Ruby Programming Projects/Final Project: Chess/lib/game.rb
+++ Ruby Programming Projects/Final Project: Chess/lib/game.rb
@@ -10,11 +10,19 @@ class Game < Board
   end
 
   def move_piece from, to
-    return false if @board[from[0].to_i][LETTERS_TO_INDEX[from[1]]] == '-'
+    return false if @board[from[0].to_i][LETTERS_TO_INDEX[from[1]]] == '-' or out_of_bounds(from, to)
 
     @board[to[0].to_i][LETTERS_TO_INDEX[to[1]]] = @board[from[0].to_i][LETTERS_TO_INDEX[from[1]]]
     @board[from[0].to_i][LETTERS_TO_INDEX[from[1]]] = '-'
     return true
+  end
+
+  def out_of_bounds from, to
+    return true if not from[0].to_i.between?(0, COLS-1)
+    return true if not to[0].to_i.between?(0, COLS-1)
+    return true if not LETTERS_TO_INDEX[from[1]]
+    return true if not LETTERS_TO_INDEX[to[1]]
+    return false
   end
 
 end
