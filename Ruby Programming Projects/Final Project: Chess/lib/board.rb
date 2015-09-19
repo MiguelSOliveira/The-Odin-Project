1a06c00fac2038dd4d8c6ae798ff75e9e9735259
Miguel <jmsoliv95@gmail.com>
2015-09-19 20:43:06 +0100

    add initialize function

--- Ruby Programming Projects/Final Project: Chess/lib/board.rb
+++ Ruby Programming Projects/Final Project: Chess/lib/board.rb
@@ -15,5 +15,15 @@ class Board
     @board = Array.new(COLS) { Array.new(ROWS, "-") }
   end
 
+  def initialize
+    @board = Array.new(COLS) { Array.new(ROWS, "-") }
+
+    @board[0][0], @board[0][1], @board[0][2], @board[0][3], @board[0][4], @board[0][5], @board[0][6], @board[0][7] = ROOK_WHITE, KNIGHT_WHITE, BISHOP_WHITE, KING_WHITE, QUEEN_WHITE, BISHOP_WHITE, KNIGHT_WHITE, ROOK_WHITE
+    @board[1][0], @board[1][1], @board[1][2], @board[1][3], @board[1][4], @board[1][5], @board[1][6], @board[1][7] = PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, PAWN_WHITE, PAWN_WHITE
+
+    @board[7][0], @board[7][1], @board[7][2], @board[7][3], @board[7][4], @board[7][5], @board[7][6], @board[7][7] = ROOK_BLACK, KNIGHT_BLACK, BISHOP_BLACK, KING_BLACK, QUEEN_BLACK, BISHOP_BLACK, KNIGHT_BLACK, ROOK_BLACK
+    @board[6][0], @board[6][1], @board[6][2], @board[6][3], @board[6][4], @board[6][5], @board[6][6], @board[6][7] = PAWN_BLACK, PAWN_BLACK, PAWN_BLACK, PAWN_BLACK, PAWN_BLACK, PAWN_BLACK, PAWN_BLACK, PAWN_BLACK
+  end
+
   
 end
