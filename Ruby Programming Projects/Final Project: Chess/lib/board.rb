8ca137ffe99113d435e245d7a27c706663ee80ad
Miguel <jmsoliv95@gmail.com>
2015-09-19 23:00:53 +0100

    moved some functions from game to board

--- Ruby Programming Projects/Final Project: Chess/lib/board.rb
+++ Ruby Programming Projects/Final Project: Chess/lib/board.rb
@@ -40,4 +40,45 @@ class Board
     end
     puts "    A   B   C   D   E   F   G   H"
   end
+
+  def out_of_bounds from, to
+    return true if not from[0].to_i.between?(0, COLS-1)
+    return true if not to[0].to_i.between?(0, COLS-1)
+    return true if not LETTERS_TO_INDEX[from[1]]
+    return true if not LETTERS_TO_INDEX[to[1]]
+    return false
+  end
+
+  def get_piece_at square
+    piece = @board[square[0].to_i][LETTERS_TO_INDEX[square[1]]]
+    case piece
+    when '-'
+      return '-'
+    when PAWN_WHITE
+      return "PAWN_WHITE"
+    when ROOK_WHITE
+      return "ROOK_WHITE"
+    when KNIGHT_WHITE
+      return "KNIGHT_WHITE"
+    when BISHOP_WHITE
+      return "BISHOP_WHITE"
+    when KING_WHITE
+      return "KING_WHITE"
+    when QUEEN_WHITE
+      return "QUEEN_WHITE"
+    when PAWN_BLACK
+      return "PAWN_BLACK"
+    when ROOK_BLACK
+      return "ROOK_BLACK"
+    when KNIGHT_BLACK
+      return "KNIGHT_BLACK"
+    when BISHOP_BLACK
+      return "BISHOP_BLACK"
+    when KING_BLACK
+      return "KING_BLACK"
+    when QUEEN_BLACK
+      return "QUEEN_BLACK"
+    end
+  end
+
 end
