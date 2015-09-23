e21041b10e2fcb2469b05ebd08e18ecc85d688c9
Miguel <jmsoliv95@gmail.com>
2015-09-23 21:52:28 +0100

    add has_checked function

--- Ruby Programming Projects/Final Project: Chess/lib/board.rb
+++ Ruby Programming Projects/Final Project: Chess/lib/board.rb
@@ -24,7 +24,7 @@ class Board
   LETTERS_TO_INDEX = { 'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7 }
   KING_WHITE, QUEEN_WHITE, ROOK_WHITE, BISHOP_WHITE, KNIGHT_WHITE = "\u2654".encode("utf-8"), "\u2655".encode("utf-8"), "\u2656".encode("utf-8"), "\u2657".encode("utf-8"), "\u2658".encode("utf-8")
   KING_BLACK, QUEEN_BLACK, ROOK_BLACK, BISHOP_BLACK, KNIGHT_BLACK = "\u265A".encode("utf-8"), "\u265B".encode("utf-8"), "\u265C".encode("utf-8"), "\u265D".encode("utf-8"), "\u265E".encode("utf-8")
-  attr_accessor :board
+  attr_accessor :board, :king_white, :king_black
 
   def clear_board
     @board = Array.new(COLS) { Array.new(ROWS, "-") }
@@ -32,6 +32,8 @@ class Board
 
   def initialize
     @board = Array.new(COLS) { Array.new(ROWS, "-") }
+    @king_white = "0D"
+    @king_black = "7D"
 
     @board[0][0], @board[0][1], @board[0][2], @board[0][3], @board[0][4], @board[0][5], @board[0][6], @board[0][7] = ROOK_WHITE, KNIGHT_WHITE, BISHOP_WHITE, KING_WHITE, QUEEN_WHITE, BISHOP_WHITE, KNIGHT_WHITE, ROOK_WHITE
     @board[1][0], @board[1][1], @board[1][2], @board[1][3], @board[1][4], @board[1][5], @board[1][6], @board[1][7] = Pawn.new("WHITE"), Pawn.new("WHITE"), Pawn.new("WHITE"), Pawn.new("WHITE"), Pawn.new("WHITE"), Pawn.new("WHITE"), Pawn.new("WHITE"), Pawn.new("WHITE")
@@ -41,6 +43,7 @@ class Board
   end
 
   def print_board
+    puts "           W H I T E             "
     puts "    A   B   C   D   E   F   G   H"
     @board.each_with_index do |row, index|
       print "#{index} | "
@@ -54,6 +57,7 @@ class Board
       puts "#{index}"
     end
     puts "    A   B   C   D   E   F   G   H"
+    puts "           B L A C K             "
   end
 
   def out_of_bounds from, to
@@ -65,9 +69,12 @@ class Board
   end
 
   def get_piece_at square
+    if square.instance_of?(Pawn)
+      return "PAWN_#{square.colour}"
+    end
+
     piece = @board[square[0].to_i][LETTERS_TO_INDEX[square[1]]]
-    white_pawn = Pawn.new("WHITE")
-    black_pawn = Pawn.new("BLACK")
+
     case piece
     when '-'
       return '-'
@@ -213,8 +220,9 @@ class Board
     return ( new_index + new_letter )
   end
 
-  def check_empty_square possible_moves, cur_square
-    if @board[cur_square[0].to_i][LETTERS_TO_INDEX[cur_square[1]]] == '-'
+  def check_empty_or_enemy_square possible_moves, cur_square, colour
+    piece = @board[cur_square[0].to_i][LETTERS_TO_INDEX[cur_square[1]]]
+    if piece == '-'
       possible_moves.push cur_square
     end
     return possible_moves
@@ -228,29 +236,43 @@ class Board
     # 0A, 1B diagonal
     while cur_square[0].to_i >= 0 and LETTERS_TO_INDEX[cur_square[1]] >= 0
       cur_square = decrement_both cur_square
-      possible_moves = check_empty_square(possible_moves, cur_square)
+      possible_moves = check_empty_or_enemy_square(possible_moves, cur_square, get_colour_at(from))
       break if decrement_both(cur_square) == cur_square
     end
     cur_square = from
     # 7A, 6B diagonal
     while cur_square[0].to_i <= 7 and LETTERS_TO_INDEX[cur_square[1]] >= 0
       cur_square = decrement_letters cur_square
-      possible_moves = check_empty_square(possible_moves, cur_square)
+      possible_moves = check_empty_or_enemy_square(possible_moves, cur_square, get_colour_at(from))
       break if decrement_letters(cur_square) == cur_square
     end
     cur_square = from
     # 7H, 6G diagonal
     while cur_square[0].to_i <= 7 and LETTERS_TO_INDEX[cur_square[1]] <= 7
       cur_square = increment_both cur_square
-      possible_moves = check_empty_square(possible_moves, cur_square)
+      possible_moves = check_empty_or_enemy_square(possible_moves, cur_square, get_colour_at(from))
       break if increment_both(cur_square) == cur_square
     end
     cur_square = from
     # 0H, 1G diagonal
     while cur_square[0].to_i >= 0 and LETTERS_TO_INDEX[cur_square[1]] <= 7
       cur_square = decrement_index cur_square
-      possible_moves = check_empty_square(possible_moves, cur_square)
+      possible_moves = check_empty_or_enemy_square(possible_moves, cur_square, get_colour_at(from))
       break if decrement_index(cur_square) == cur_square
+    end
+
+    top_left_square     =  (to[0].to_i - 1).to_s + (to[1].ord-1).chr
+    top_right_square    =  (to[0].to_i - 1).to_s + (to[1].ord+1).chr
+    bottom_left_square  =  (to[0].to_i + 1).to_s + (to[1].ord-1).chr
+    bottom_right_square =  (to[0].to_i + 1).to_s + (to[1].ord+1).chr
+    if possible_moves.include?(top_left_square) and get_colour_at(top_left_square) != get_colour_at(to)
+      possible_moves.push to
+    elsif possible_moves.include?(top_right_square) and get_colour_at(top_right_square) != get_colour_at(to)
+      possible_moves.push to
+    elsif possible_moves.include?(bottom_left_square) and get_colour_at(bottom_left_square) != get_colour_at(to)
+      possible_moves.push to
+    elsif possible_moves.include?(bottom_right_square) and get_colour_at(bottom_right_square) != get_colour_at(to)
+      possible_moves.push to
     end
     return true if possible_moves.include?(to)
     return false
@@ -288,7 +310,33 @@ class Board
       return false
     end
   end
-end
 
-g = Board.new
-g.board[4][4] = Pawn.new("WHITE")
+  def has_checked square
+    if get_colour_at(square) == "WHITE"
+      cur_king = @king_black
+    else
+      cur_king = @king_white
+    end
+
+    piece = get_piece_at(square).split('_')[0]
+    case piece
+    when "BISHOP"
+      return true if valid_play_for_bishop(square, cur_king)
+      return false
+    when "ROOK"
+      return true if valid_play_for_rook(square, cur_king)
+      return false
+    when "KNIGHT"
+      return true if valid_play_for_knight(square, cur_king)
+      return false
+    when "QUEEN"
+      return true if valid_play_for_queen(square, cur_king)
+      return false
+    when "PAWN"
+      return true if valid_play_for_pawn(@board[square[0].to_i][LETTERS_TO_INDEX[square[1]]], square, cur_king)
+      return false
+    end
+  end
+
+  
+end
